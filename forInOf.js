fe7de8271f452bb777e7ae2d42f917d98dd7f69b
iCodeRx <arababs2016@gmail.com>
2021-03-27 20:21:35 +0100

    initial commit

---
+++ forInOf.js
@@ -0,0 +1,79 @@
+/*
+for..in can also be used in arrays, at this time the array is 
+treated as an object. Modify the code above, we can see:
+function showObjectKeys(obj){
+          for (var key in obj){
+                    console.log(key);
+          }
+}
+
+function showObjectValues(obj){
+          for (var key in obj){
+                    console.log(obj[key]);
+          }
+}
+
+var arr=["one","two","three"];
+console.log("keys of arr:")
+showObjectKeys(arr);
+console.log("values of arr:")
+showObjectValues(arr);
+
+Code results:
+keys of arr:
+0
+1
+2
+
+values of arr:
+one
+two
+three
+We can see, keys is the index of the array elements. Need attention: 
+When using for..in with an array, key(index) is always a string, not a 
+number. In the example above, keys is "0", "1" and "2", we can't see 
+the quotes because console.log() doesn't show it.
+
+Although for..in can traverse the array, but we do not recommend the 
+use of it. Because it has a flaw, in some cases, it may not be in 
+accordance with the order of the array elements to traverse the array. 
+So we recommend you use another variant:for..of(New member of ES6), 
+it can traverse all the values of the array, if you don't care about its index.
+
+***************************
+Coding in function giveMeFive, function accept 1 parameter:obj, it's 
+an object. You need to traverse the obj, if the length of the object 
+key equals to 5, then push the key value to the array (you need to define 
+the array by yourself, this time I won't help you). Additionally push the 
+value to the array as well, if the length of the value is equal to 5.
+Return the five after works finished.
+*/
+let obj = {
+          ondo: "Akure",
+          osun: "Osogbo",
+          oyo: "Ibadan",
+          ekiti: "Ado-Ekiti",
+          ogun: "Abeokuta",
+          lagos: "Ikeja"
+}
+
+function giveMeFive(input){
+          let arr = [];
+
+          for(key in input) {
+
+                    // if(key.length == 5){
+                    //           arr.push(key)
+                    // }else if(input[key].length == 5){
+                    //           arr.push(input[key])
+                    // }
+
+                    key.length == 5 ? arr.push(key) : input[key].length == 5 ?  arr.push(input[key]) : null;
+          }
+          
+          return arr;
+}
+
+console.log(giveMeFive(obj));
+
+
